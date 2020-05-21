80e0bcbd36efe36bca67ec4a72d14bbfe1ceeba4
I9hdkill <jonathanfriedrich@live.de>
2020-05-21 17:04:51 +0200

    Convert Project to maven

---
+++ src/main/java/com/moneybags/tempfly/TempFlyAPI.java
@@ -0,0 +1,105 @@
+package com.moneybags.tempfly;
+
+import java.util.UUID;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+import com.moneybags.tempfly.fly.FlyHandle;
+import com.moneybags.tempfly.fly.Flyer;
+import com.moneybags.tempfly.time.TimeHandle;
+
+public class TempFlyAPI {
+	
+	/**
+	 * @param player Player uuid
+	 * @return The amount of flight in seconds a player has.
+	 */
+	public double getFlightTime(UUID player) {
+		return TimeHandle.getTime(player);
+	}
+	
+	/**
+	 * Set the flight time of a player in seconds.
+	 * @param player Player uuid
+	 */
+	public void setFlightTime(UUID player, double seconds) {
+		TimeHandle.setTime(player, seconds);
+	}
+	
+	/**
+	 * Add flight time in seconds
+	 * @param player Player uuid
+	 * @param seconds Seconds to give the player
+	 */
+	public void addFlightTime(UUID player, double seconds) {
+		TimeHandle.addTime(player, seconds);
+	}
+	
+	/**
+	 * Remove flight time from the player.
+	 * @param player Player uuid
+	 * @param seconds Seconds to remove from the player
+	 */
+	public void removeFlightTime(UUID player, double seconds) {
+		TimeHandle.removeTime(player, seconds);
+	}
+	
+	/**
+	 * 
+	 * @param loc The location to check
+	 * @return True if a player is allowed to fly at the given location.
+	 */
+	public boolean canFlyAt(Location loc) {
+		return FlyHandle.flyAllowed(loc);
+	}
+	
+	/**
+	 * 
+	 * @return All flyer objects for players using TempFly.
+	 */
+	public Flyer[] getAllFlyers() {
+		return FlyHandle.getFlyers();
+	}
+	
+	/**
+	 * 
+	 * @param p
+	 * @return The flyer object if one exists. Null if the player is not using TempFly.
+	 */
+	public Flyer getFlyer(Player p) {
+		return FlyHandle.getFlyer(p);
+	}
+	
+	/**
+	 * 
+	 * @param p Player to toggle
+	 * @param enabled Toggle enabled true or false
+	 * @param fallDamage If true the player will take fall damage when their flight is toggled off.
+	 */
+	public void toggleTempfly(Player p, boolean enabled, boolean fallDamage) {
+		if (enabled) {
+			FlyHandle.removeFlyer(p);
+			if (!fallDamage) FlyHandle.addDamageProtection(p);
+		} else {
+			FlyHandle.removeDamageProtction(p);
+			FlyHandle.addFlyer(p);
+		}
+	}
+	
+	/**
+	 * Force TempFly to process a combat tag.
+	 * This will use all the settings in the config like normal combat.
+	 * Useful for plugins that manually handle entity damage and do not
+	 * allow entities to directly harm each other. For instance, if a plugin
+	 * has a custom health system and deals damage on combat with .damage(), TempFly
+	 * would have no way to know this combat occured unless you use this.
+	 * 
+	 * @param victim The entity that got attacked
+	 * @param actor The attacking entity
+	 */
+	public void processCombat(Entity victim, Entity actor) {
+		FlyHandle.processCombat(victim, actor);
+	}
+} 
---
+++ src/main/java/com/moneybags/tempfly/command/player/CmdSpeed.java
@@ -0,0 +1,102 @@
+package com.moneybags.tempfly.command.player;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissionAttachmentInfo;
+
+import com.moneybags.tempfly.util.U;
+import com.moneybags.tempfly.util.V;
+
+public class CmdSpeed {
+
+	public CmdSpeed(CommandSender s, String[] args) {
+		
+		Player p = null;
+		float speed = 1;
+		
+		if (args.length > 2) {
+			if (!U.hasPermission(s, "tempfly.speed.other")) {
+				U.m(s, V.invalidPermission);
+				return;
+			}
+			p = Bukkit.getPlayer(args[1]);
+			if (p == null) {
+				U.m(s, V.invalidPlayer.replaceAll("\\{PLAYER}", args[1]));
+				return;
+			}
+			
+			try {
+				speed = Float.parseFloat(args[2]);
+			} catch (Exception e) {
+				U.m(s, V.invalidNumber);
+				return;
+			}
+			
+			
+		} else if (args.length > 1) {
+			if (!U.isPlayer(s)) {
+				U.m(s, V.invalidSender);
+				return;
+			}
+			if (!U.hasPermission(s, "tempfly.speed.self")) {
+				U.m(s, V.invalidPermission);
+				return;
+			}
+			p = (Player)s;
+			
+			try {
+				speed = Float.parseFloat(args[1]);
+			} catch (Exception e) {
+				U.m(s, V.invalidNumber);
+				return;
+			}
+			
+		} else {
+			U.m(s, U.cc("/tf speed [speed]"));
+			return;
+		}
+		
+		
+		if (speed < 0) {
+			speed = 0;
+		} else if (speed > 10) {
+			speed = 10;
+		}
+		
+		if (!p.isOp()) {
+			float max = 1;
+			for (PermissionAttachmentInfo info: p.getEffectivePermissions()) {
+				String perm = info.getPermission();
+				if (perm.startsWith("tempfly.speed")) {
+					String[] split = perm.split("\\.");
+					try {
+						float found = Float.parseFloat(split[2]);
+						if (found > max) {
+							max = found;
+						}
+					} catch (Exception e) {
+						continue;
+					}
+				}
+			}
+			if (speed > max) {
+				speed = max;
+			}
+			if (speed > 10) {
+				speed = 10;
+			}
+		}
+		
+		float fin = (float)(speed * 0.1);
+		p.setFlySpeed(fin);
+		U.m(p, V.flySpeedSelf
+				.replaceAll("\\{SPEED}", String.valueOf(speed)));
+		if (!s.equals(p)) {
+			U.m(s, V.flySpeedOther
+					.replaceAll("\\{SPEED}", String.valueOf(speed))
+					.replaceAll("\\{PLAYER}", p.getName()));
+		}
+	}
+	
+}
